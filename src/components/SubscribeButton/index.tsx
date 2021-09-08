9fd138a5a5473aadda50f336a609ef6af5d482d7
calixoto <grimmjow006@live.com>
2021-09-08 19:35:04 -0300

    butao assinar corrigido

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,18 +2,14 @@ import Link from 'next/link';
 import styles from './styles.module.scss';
 
 export function SubscribeButton() {
-    let isMobile = false;
-
-    if (screen.width < 720) {
-        isMobile = true;
-    }
-    return isMobile ? (
-        <Link href="https://api.whatsapp.com/send?phone=558488658904&text=">
-            <a target="_blank" className={styles.subscribeButton}>Assine agora</a>
-        </Link>
-    ) : (
-        <Link href="https://web.whatsapp.com/send?phone=558488658904">
-            <a target="_blank" className={styles.subscribeButton}>Assine agora</a>
-        </Link>
+    return (
+        <>
+            <Link href="https://api.whatsapp.com/send?phone=558488658904&text=">
+                <a target="_blank" className={styles.subscribeButtonMobile}>Assine agora</a>
+            </Link>
+            <Link href="https://web.whatsapp.com/send?phone=558488658904&text=">
+                <a target="_blank" className={styles.subscribeButton}>Assine agora</a>
+            </Link>
+        </>
     )
 }

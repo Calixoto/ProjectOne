e95d2cde2d37bd5cb9c4ff21122e4db4ceac46fa
calixoto <grimmjow006@live.com>
2021-09-09 12:25:34 -0300

    tag a no infobar

--- src/components/InfoBar/index.tsx
+++ src/components/InfoBar/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './styles.module.scss';
 
 export function InfoBar() {
@@ -8,51 +9,68 @@ export function InfoBar() {
                     <h2>
                         SAnet
                     </h2>
-                    <span>
+                    <a>
                         Sobre nós
-                    </span>
-                    <span>
+                    </a>
+                    <a>
                         Perguntas frequentes
-                    </span>
+                    </a>
                 </section>
                 <section>
                     <h2>
                         Planos
                     </h2>
-                    <span>
-                        SA Família
-                    </span>
-                    <span>
-                        SA Master
-                    </span>
-                    <span>
-                        SA Ultra
-                    </span>
+                    <Link href="/planos">
+                        <a>
+                            SA Família
+                        </a>
+                    </Link>
+                    <Link href="/planos">
+                        <a>
+                            SA Master
+                        </a>
+                    </Link>
+                    <Link href="/planos">
+                        <a>
+                            SA Ultra
+                        </a>
+                    </Link>
                 </section>
                 <section>
                     <h2>
                         Contatos
                     </h2>
-                    <span>
-                        0800 608 6236
-                    </span>
-                    <span>
-                        meajuda@sanet.com.br
-                    </span>
-                    <span>
-                        WhatsApp
-                    </span>
-                    <span>
-                        Instagram
-                    </span>
+                    <a>
+                        (84) 98865-8904
+                    </a>
+                    <a>
+                        sanetprovedor@gmail.com
+                    </a>
+                    <Link href="https://api.whatsapp.com/send?phone=558488658904&text=">
+                        <a target="_blank" className={styles.isMobile}>
+                            WhatsApp
+                        </a>
+                    </Link>
+                    <Link href="https://web.whatsapp.com/send?phone=558488658904&text=">
+                        <a target="_blank" className={styles.isDesktop}>
+                            WhatsApp
+                        </a>
+                    </Link>
+                    <Link href="https://instagram.com/sanetprovedor?utm_medium=copy_link">
+                        <a target="_blank">
+                            Instagram
+                        </a>
+                    </Link>
                 </section>
             </div>
             <section>
                 <img src="/images/logosanet.png" alt="logo Sanet" />
                 <h3>
-                    xx.xxx.xxx/xxxx-xx
+                    037.714.943/0001-96
                     <br />
-                    Rua ---------,-- Natal,RN - xxxxx-xxx
+                    Rua do Bambelô, 858A Natal
+                    <br />
+                    RN - 59139-160
                 </h3>
             </section>
         </div>
